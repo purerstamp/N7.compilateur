d3570fa269ee63dacead12d1c0aa2c54aefe81f3
Professeur Tournesol <mael-soulie@hotmail.fr>
2019-02-25 09:54:52 +0100

    ménage git add .!

--- rat/src/attributs/Info.java
+++
@@ -1,9 +0,0 @@
-
-package attributs;
-
-/** Information associée à un symbole dans la table des symboles.
- *  Ceci n'est qu'une interface de marquage. 
- */
-public interface Info {
-	
-}
--- rat/src/attributs/Operateur.java
+++
@@ -1,8 +0,0 @@
-package attributs;
-
-/** Les opérateurs logiques et arithmétiques.
- *  NONE est en fait l'opérateur identité (ou plus exactement de projection sur son premier argument). 
- */
-public enum Operateur {
-    PLUS, MULT, EGAL, INFERIEUR, NONE;
-}
